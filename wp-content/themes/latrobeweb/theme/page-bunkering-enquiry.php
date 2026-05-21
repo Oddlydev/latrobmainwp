8f1431f5397aa28da83e1946392ef90c4744ab16
Gayali Hasara <gayali@oddly.co>
2026-05-21 16:52:59 +0530

    Update page-bunkering-enquiry.php

--- wp-content/themes/latrobeweb/theme/page-bunkering-enquiry.php
+++ wp-content/themes/latrobeweb/theme/page-bunkering-enquiry.php
@@ -22,7 +22,7 @@ get_header();
 					<?php else : ?>
 						<div class="la-bunker-empty-state">
 							<h1><?php esc_html_e( 'Bunkering Enquiry', 'latrobeweb' ); ?></h1>
-							<p><?php esc_html_e( 'Add your Contact Form 7 shortcode to this page content to render the multi-step form.', 'latrobeweb' ); ?></p>
+							<p><?php esc_html_e( '[contact-form-7 id="75c4d43" title="Contact form 1"]', 'latrobeweb' ); ?></p>
 						</div>
 					<?php endif; ?>
 				</div>
